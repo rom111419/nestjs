aa5569d2cad530c0bfe80abb4232539c1f10fbd3
Roman <info@rit-m.by>
2021-08-24 15:56:19 +0300

    PUFFI updates

--- src/modules/devi-bag/devi-bag-images/devi-bag-images.controller.ts
+++ src/modules/devi-bag/devi-bag-images/devi-bag-images.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
-import { readFileSync } from "fs";
+import { Controller, Get, Param } from '@nestjs/common';
+import { readFileSync } from 'fs';
 
 @Controller('devi-bag-images')
 export class DeviBagImagesController {
   @Get()
   getHello(): any[] {
     const file = readFileSync('settings/devi-bag/devi-bag-images.json', {
-      encoding: 'utf8'
-    })
+      encoding: 'utf8',
+    });
     return JSON.parse(file);
+  }
+
+  @Get('/titleImages/:id')
+  findAllByTagId(@Param('id') tagId: string): any[] {
+    const file = readFileSync('settings/devi-bag/devi-bag-images.json', {
+      encoding: 'utf8',
+    });
+    return JSON.parse(file).filter(
+      (it) => it.tagsId.includes(tagId) && it.isTitle,
+    );
+  }
+
+  @Get('/detail/:id')
+  findByImageId(@Param('id') productId: string): any[] {
+    const file = readFileSync('settings/devi-bag/devi-bag-images.json', {
+      encoding: 'utf8',
+    });
+    return JSON.parse(file).filter((it) => it.productIds.includes(productId));
   }
 }
--- src/modules/devi-bag/devi-bag-tags/devi-bag-tags.controller.ts
+++ src/modules/devi-bag/devi-bag-tags/devi-bag-tags.controller.ts
@@ -1,28 +1,32 @@
-import { Controller, Get, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
-import { FileInterceptor } from "@nestjs/platform-express";
-import { JSON_FILE_FILTER } from "../utils/image-file.filter";
-import {readFileSync} from 'fs'
-import { json } from "express";
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { JSON_FILE_FILTER } from '../utils/image-file.filter';
+import { readFileSync } from 'fs';
 
 @Controller('devi-bag-tags')
 export class DeviBagTagsController {
   @Get()
   getDeviBagTags(): any[] {
     const file = readFileSync('settings/devi-bag/devi-bag-tags.json', {
-      encoding: 'utf8'
-    })
+      encoding: 'utf8',
+    });
     return JSON.parse(file);
   }
 
   @Post('upload')
   @UseInterceptors(
     FileInterceptor('file', {
-      fileFilter: JSON_FILE_FILTER
+      fileFilter: JSON_FILE_FILTER,
     }),
   )
   uploadFile(@UploadedFile() file) {
-    console.log(file);
+    console.log([file]);
     return file;
   }
-
 }
