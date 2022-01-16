04a616e46cdcb1552be35203baf70acff09489ea
Roman Zhukov <nortall.ritmby@gmail.com>
2022-01-16 16:41:05 +0300

    npm updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeviBagModule } from './modules/devi-bag/devi-bag.module';
-import { DbModule } from './modules/db/db.module';
+/*import { DbModule } from './modules/db/db.module';
 
-/*import { TextsModule } from './modules/texts/texts.module';*/
+import { TextsModule } from './modules/texts/texts.module';*/
 
 @Module({
   imports: [DeviBagModule /* , DbModule , TextsModule */],
